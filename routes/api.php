36cf9e70a96b2a23cd33c43c9bf1cbe7985315fb
eggnocent <egiwiratamaaa@gmail.com>
2024-05-29 05:56:31 +0700

    29/05/24

--- routes/api.php
+++ routes/api.php
@@ -6,31 +6,19 @@ use App\Http\Controllers\KaryawanController;
 use App\Http\Controllers\PekerjaanController;
 use App\Http\Controllers\DivisiController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Authenticated Routes
 Route::middleware('auth.jwt')->group(function () {
-    // Routes for Karyawan
+    // Routes untuk Karyawan
     Route::resource('karyawans', KaryawanController::class)->except(['create', 'edit']);
     Route::get('karyawan-stats', [KaryawanController::class, 'stats']);
 
-    // Routes for Pekerjaan
+    // Routes untuk Pekerjaan
     Route::resource('pekerjaans', PekerjaanController::class)->except(['create', 'edit']);
 
-    // Routes for Divisi
+    // Routes untuk Divisi
     Route::resource('divisis', DivisiController::class)->except(['create', 'edit']);
 });
 
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Rute untuk mengakses app.blade.php
 Route::get('/', function () {
     return view('app');
 });
 
-// Rute lainnya yang membutuhkan otentikasi
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('karyawans', KaryawanController::class);
     Route::resource('pekerjaans', PekerjaanController::class);
     Route::resource('divisis', DivisiController::class);
 });
 
-// Rute fallback untuk menangani halaman tidak ditemukan
 Route::fallback(function () {
-    return Inertia::render('NotFound'); // Pastikan Anda memiliki komponen NotFound.vue
+    return Inertia::render('NotFound');
 });
